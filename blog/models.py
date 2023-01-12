5944a47c70bd73f9f25b13966d0e4bd8ff65d986
Vic Zhang <z2510655@gmail.com>
2023-01-12 23:07:26 +1000

    add views and templates

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
-from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=20)
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -12,9 +13,13 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     author = models.CharField(max_length=60)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    def __str__(self):
+        return "Comment by %s on %s"%(self.author,self.created_on.strftime("%m/%d/%Y"))
