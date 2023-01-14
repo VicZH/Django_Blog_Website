40c422c67cb62ed4b8fddbdba244b42b875024d7
Vic Zhang <z2510655@gmail.com>
2023-01-14 22:08:46 +1000

    add login logout buttom on the nav bar

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("projects/", include("projects.urls")),
     path("blog/", include("blog.urls")),
+    path("", include("blog.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
